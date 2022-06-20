81fd4eb7b4506cd5be21d6128aebcac73b64bf44
carloseur24 <carloseur24@gmail.com>
2022-06-20 16:35:57 -0400

    test1

--- src/components/Title.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Title = () => {
-    return ( 
-    <h1 className='title'> TO DO TASK APP </h1>
-    )
-}
-
-export default Title
--- src/components/index.jsx
+++
@@ -1,7 +0,0 @@
-export {default as CreateTodoButton} from './CreateTodoButton';
-export {default as CreateTodoButtonText} from './CreateTodoButtonText';
-export {default as TodoCounter} from './TodoCounter';
-export {default as TodoList} from './TodoList';
-export {default as TodoSearch} from './TodoSearch';
-export {default as TodoItem} from './TodoItem';
-export {default as Header} from './Header';
