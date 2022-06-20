04c3aa5635324e25b07e557616a949e9b6fba711
carloseur24 <carloseur24@gmail.com>
2022-06-20 16:58:43 -0400

    test4

--- src/index.js
+++ src/index.js
@@ -3,15 +3,22 @@ import ReactDOM from 'react-dom/client';
 import {AppUI} from './AppUI';
 import {TodoProvider} from './TodoContext'
 import './styles/index.css'
+import { BrowserRouter,
+  Routes,
+  Route} from "react-router-dom"
 
 
 const root = ReactDOM.createRoot(
   document.getElementById('root')
 );
 root.render( 
+  <BrowserRouter>
   <React.StrictMode>
     <TodoProvider>
-          <AppUI />
+      <Routes>
+          <Route path='/' element={<AppUI/>} />
+      </Routes>
     </TodoProvider>
   </React.StrictMode>
+  </BrowserRouter>
 );
